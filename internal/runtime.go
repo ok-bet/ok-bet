d113083ad791ae62310fde6bc0a82a7ab67c7e62
ExoMonk <0xexomonk@gmail.com>
2024-06-02 16:24:00 +0100

    fix: settlement race condition

--- internal/runtime.go
+++ internal/runtime.go
@@ -1,3 +1,4 @@
 // 2024-06-01T11:50:00 feat: market resolver
 // 2024-06-02T14:15:00 feat: add market analytics
 // 2024-06-02T17:25:00 refactor: prediction pipeline
+// 2024-06-02T16:24:00 fix: settlement race condition
