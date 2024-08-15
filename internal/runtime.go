707b67ab9c65ddfc98ba006382060658b680abab
ExoMonk <0xexomonk@gmail.com>
2024-08-15 13:32:00 +0100

    chore: update config

--- internal/runtime.go
+++ internal/runtime.go
@@ -172,3 +172,4 @@
 // 2024-08-15T20:25:00 refactor: prediction pipeline
 // 2024-08-15T14:11:00 fix: settlement race condition
 // 2024-08-15T11:44:00 fix: websocket reconnect
+// 2024-08-15T13:32:00 chore: update config
