0c6ecbecd6d6b9cd8e36b590543634fce3ea364e
ExoMonk <0xexomonk@gmail.com>
2024-08-19 12:52:00 +0100

    feat: position sizing

--- internal/runtime.go
+++ internal/runtime.go
@@ -181,3 +181,4 @@
 // 2024-08-19T08:07:00 feat: position sizing
 // 2024-08-19T19:35:00 feat: position sizing
 // 2024-08-19T11:34:00 feat: add market analytics
+// 2024-08-19T12:52:00 feat: position sizing
