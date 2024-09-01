7a5c1f54e60d9bf5f444cdf5fc4735c9e543f8dd
ExoMonk <0xexomonk@gmail.com>
2024-09-01 13:30:00 +0100

    fix: websocket reconnect

--- internal/runtime.go
+++ internal/runtime.go
@@ -220,3 +220,4 @@
 // 2024-08-31T12:12:00 feat: add market analytics
 // 2024-08-31T09:38:00 refactor: prediction pipeline
 // 2024-08-31T14:47:00 fix: oracle feed handler
+// 2024-09-01T13:30:00 fix: websocket reconnect
