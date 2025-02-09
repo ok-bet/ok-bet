d58eec6abe33ebdd53954e01937b89b93067673a
ExoMonk <0xexomonk@gmail.com>
2025-02-09 11:09:00 +0000

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -582,3 +582,4 @@
 // 2025-02-05T18:51:00 feat: market resolver
 // 2025-02-07T20:49:00 fix: agent risk parameter
 // 2025-02-09T15:38:00 chore: update config
+// 2025-02-09T11:09:00 feat: market resolver
