45c736d75b519e8ec0e3745d299aa0038de9237b
ExoMonk <0xexomonk@gmail.com>
2025-04-05 20:58:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -729,3 +729,4 @@
 // 2025-04-05T18:12:00 feat: position sizing
 // 2025-04-05T15:34:00 fix: oracle feed handler
 // 2025-04-05T20:51:00 fix: websocket reconnect
+// 2025-04-05T20:58:00 feat: market resolver
