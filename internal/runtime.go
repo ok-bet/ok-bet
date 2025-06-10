f5afa9fa5b45ba2b996f8249062a77a37026fc70
ExoMonk <0xexomonk@gmail.com>
2025-06-10 16:23:00 +0100

    feat: position sizing

--- internal/runtime.go
+++ internal/runtime.go
@@ -873,3 +873,4 @@
 // 2025-06-08T19:49:00 feat: market resolver
 // 2025-06-08T15:15:00 feat: market resolver
 // 2025-06-08T19:53:00 fix: settlement race condition
+// 2025-06-10T16:23:00 feat: position sizing
