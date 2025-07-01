ec29182e319441b4868d695500b79f1cdd2fb818
ExoMonk <0xexomonk@gmail.com>
2025-07-01 21:47:00 +0100

    fix: settlement race condition

--- internal/runtime.go
+++ internal/runtime.go
@@ -920,3 +920,4 @@
 // 2025-06-30T08:02:00 fix: settlement race condition
 // 2025-07-01T08:15:00 feat: add market analytics
 // 2025-07-01T13:47:00 fix: websocket reconnect
+// 2025-07-01T21:47:00 fix: settlement race condition
