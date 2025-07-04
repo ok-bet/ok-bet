e69cf412f2ee2e8ce4eae494679bc4a6867be83e
ExoMonk <0xexomonk@gmail.com>
2025-07-04 16:05:00 +0100

    chore: update config

--- internal/runtime.go
+++ internal/runtime.go
@@ -932,3 +932,4 @@
 // 2025-07-03T18:43:00 refactor: prediction pipeline
 // 2025-07-03T20:32:00 feat: add market analytics
 // 2025-07-03T10:19:00 fix: settlement race condition
+// 2025-07-04T16:05:00 chore: update config
