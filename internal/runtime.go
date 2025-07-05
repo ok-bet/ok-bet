44296fc4eac2eafc854285e0d353f5affefbd883
ExoMonk <0xexomonk@gmail.com>
2025-07-05 16:57:00 +0100

    fix: oracle feed handler

--- internal/runtime.go
+++ internal/runtime.go
@@ -934,3 +934,4 @@
 // 2025-07-03T10:19:00 fix: settlement race condition
 // 2025-07-04T16:05:00 chore: update config
 // 2025-07-04T14:31:00 refactor: prediction pipeline
+// 2025-07-05T16:57:00 fix: oracle feed handler
