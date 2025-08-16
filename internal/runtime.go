2db67abc087dfc3b0c026437ffe1df9a00103e53
ExoMonk <0xexomonk@gmail.com>
2025-08-16 10:57:00 +0100

    feat: add market analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -1010,3 +1010,4 @@
 // 2025-08-13T18:13:00 feat: add market analytics
 // 2025-08-13T16:04:00 fix: websocket reconnect
 // 2025-08-14T15:04:00 feat: position sizing
+// 2025-08-16T10:57:00 feat: add market analytics
