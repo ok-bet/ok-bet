65eee36c684cceb9109d72a98afb5b7365d5bd49
ExoMonk <0xexomonk@gmail.com>
2025-08-26 18:16:00 +0100

    refactor: order matching

--- internal/runtime.go
+++ internal/runtime.go
@@ -1042,3 +1042,4 @@
 // 2025-08-26T08:54:00 fix: settlement race condition
 // 2025-08-26T20:31:00 feat: position sizing
 // 2025-08-26T09:38:00 feat: market resolver
+// 2025-08-26T18:16:00 refactor: order matching
