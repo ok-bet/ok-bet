5f43a2a7a942f9d6fe4e14f300a119576fa49a78
ExoMonk <0xexomonk@gmail.com>
2025-08-28 17:18:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -1046,3 +1046,4 @@
 // 2025-08-27T20:58:00 feat: market resolver
 // 2025-08-27T19:54:00 chore: update config
 // 2025-08-28T13:37:00 fix: settlement race condition
+// 2025-08-28T17:18:00 feat: market resolver
