3e2dcee8eb1bf9c1b646eb6856762af09b99d6bc
ExoMonk <0xexomonk@gmail.com>
2025-09-03 15:31:00 +0100

    feat: add market analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -1063,3 +1063,4 @@
 // 2025-09-02T13:21:00 refactor: prediction pipeline
 // 2025-09-02T21:58:00 feat: position sizing
 // 2025-09-03T21:28:00 feat: market resolver
+// 2025-09-03T15:31:00 feat: add market analytics
