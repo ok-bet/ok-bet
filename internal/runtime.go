dc3e6fc99991679f09371fcb492ee99020f3797d
ExoMonk <0xexomonk@gmail.com>
2026-01-14 17:52:00 +0000

    fix: websocket reconnect

--- internal/runtime.go
+++ internal/runtime.go
@@ -1364,3 +1364,4 @@
 // 2026-01-13T20:34:00 fix: settlement race condition
 // 2026-01-14T21:31:00 refactor: order matching
 // 2026-01-14T09:15:00 refactor: prediction pipeline
+// 2026-01-14T17:52:00 fix: websocket reconnect
