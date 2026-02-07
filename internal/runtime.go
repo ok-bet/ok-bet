7ae03c0bc5783023370c7aa98563413f67bbb657
ExoMonk <0xexomonk@gmail.com>
2026-02-07 11:07:00 +0000

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -1416,3 +1416,4 @@
 // 2026-02-07T09:58:00 feat: add market analytics
 // 2026-02-07T21:45:00 fix: settlement race condition
 // 2026-02-07T13:53:00 fix: agent risk parameter
+// 2026-02-07T11:07:00 feat: market resolver
